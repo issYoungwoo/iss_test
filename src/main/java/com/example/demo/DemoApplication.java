75bfd95beabde15920fe944b5166e5f7c5150255
skyhanul <bluesky7831@naver.com>
2019-07-06 13:04:35 +0900

    ISS EDU hanul

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+	 System.out.println();
+		
 	}
 
 }
