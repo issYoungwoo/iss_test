bce8a4e4fe3ffd7f2d4f12624ad5b50484a4f2b5
lcj225 <32259312+lcj225@users.noreply.github.com>
2019-07-06 13:03:02 +0900

    0706
    for追加

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		for(int i = 0; i< 10; i++) {
+			i += i;
+		}
 	}
 
 }
